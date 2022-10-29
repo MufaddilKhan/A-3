46782c0ab393c7149a2c30c0e5f3d495cc2d243a
mufaddil <mufaddil2003@gmail.com>
2022-10-29 05:55:13 -0700

    work complete

---
+++ index.dart
@@ -0,0 +1,82 @@
+// void main() {
+//  var list = ["AHMED", "SHAFEEQ", "UMER", "DIL", "FATIMA", "OMER"];
+
+//   print(list[0]);
+//   print(list[1]);
+//   print(list[2]);
+//   print(list[3]);
+//   print(list[4]);
+//   print(list[5]);
+  
+// }
+
+
+// void main() {
+//   List day=[];
+//   day.addAll(["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]);
+//   print(day);
+  
+  
+// }
+
+
+// void main() {
+//   List<String> name = [
+//     "AHMED",
+//     "ALI",
+//     "MUFADDIL",
+//     "MUJTABA",
+//     "MUTAHIR",
+//     "MOIZ",
+//     "OWAIS"
+//   ];
+
+//   List<String> startWithS =
+//       name.where((element) => element.startsWith("A")).toList();
+
+//   print(startWithS);
+  
+// }
+
+
+// void main() {
+// Map<String, String> countryCapital = {
+//   'USA': 'Nothing',
+//   'India': 'New Delhi',
+//   'China': 'Beijing'
+// };
+//   // Updating Item
+//   countryCapital['USA'] = 'Washington, D.C.';
+//   print(countryCapital);
+// }
+
+
+
+// void main() {
+//   Map<String, double> mathMarks = {
+//     "ram": 30,
+//     "mark": 32,
+//     "harry": 88,
+//     "raj": 69,
+//     "john": 15,
+//   };
+
+//   mathMarks.removeWhere((key, value) => value < 32);
+
+//   print(mathMarks);
+// }
+
+
+// void main() {
+//   Map<String, double> mathMarks = {
+//     "ram": 30,
+//     "mark": 32,
+//     "harry": 88,
+//     "raj": 69,
+//     "john": 15,
+//   };
+
+//   mathMarks.removeWhere((key, value) => value < 32);
+
+//   print(mathMarks);
+// }
